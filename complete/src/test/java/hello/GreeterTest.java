f1d4725ae9698da51b7d268d43aecbe356dc99a4
NaveenReeddy <naveenreddy026@gmail.com>
2023-11-21 21:20:29 +0530

    BUILD-0000:Updating java files with import statements

--- complete/src/test/java/hello/GreeterTest.java
+++ complete/src/test/java/hello/GreeterTest.java
@@ -2,7 +2,6 @@ package hello;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
 import org.junit.Test;
 
 public class GreeterTest {
